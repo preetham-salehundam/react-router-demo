2af6721ab9ab4fae0f1319580ffee17de5de1b18
502554557 <preetham.salehundam@ge.com>
2017-05-31 21:55:43 +0530

    added routes

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,13 +1,41 @@
 import React from 'react';
 import {render} from 'react-dom';
+import {BrowserRouter,Switch,Route} from 'react-router-dom';
 
 class App extends React.Component{
     constructor(props){
         super(props);
     }
     render(){
-        return(<h1>Hi</h1>);
+        return(
+            <div>
+                 {/*<Header />*/}
+                <Main />
+            </div>
+
+
+        );
+    }
+}
+class Main extends React.Component{
+    render(){
+        return(<main>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/about" component={About} />
+                </Switch>
+            </main>);
     }
 }
 
-render(<App />,document.getElementById('app'));
+class Home extends React.Component{
+    render(){
+        return <h1>Home</h1>
+    }
+}
+class About extends React.Component{
+    render(){
+        return <h1>About</h1>
+    }
+}
+render(<BrowserRouter><App /></BrowserRouter>,document.getElementById('root'));
